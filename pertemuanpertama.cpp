4a1474e7f22edd4f224269a60315ea8d37c3d7a1
LENOVO <LENOVO@LAPTOP-56DHCMP1>
2024-02-26 00:43:22 -0800

    Add project files.

---
+++ pertemuanpertama.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	//begin
+	// numeric nPanjang, nLebar, nLuas
+	int nPanjang, nLebar, nLuas;
+	// display 'Masukan Panjang= '
+	cout << "Masukan Panjang = ";
+	// accept nPanjang
+	cin >> nPanjang;
+	// display 'Masukan Lebar'
+	cout << "Masukan Lebar = ";
+	// accept nLebar
+	cin >> nLebar;
+	// compute nLuas = nPanjang * nLebar
+	nLuas = nPanjang * nLebar;
+		// display 'Luasnya adalah = ' + nLuas
+		cout << "Luasnya adalah " << nLuas << endl;
+}//end
+
+
